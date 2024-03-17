ee34d88fca26441e7e41f8d969114a40fb1250a2
SerjMonstrX <SerjMonstrX@gmail.com>
2024-03-17 22:01:13 +0700

    ДЗ 24_2 2.Добавлена модель Payment

--- users/models.py
+++ users/models.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 NULLABLE = {'blank': True, 'null': True}
+PAYMENT_METHOD_CHOICES = (
+    ('cash', 'Наличные'),
+    ('transfer', 'Перевод на счет'),
+)
+
 
 class User(AbstractUser):
     username = None
@@ -13,6 +20,23 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    payment_date = models.DateField(verbose_name='дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='оплаченный курс', **NULLABLE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='отдельно оплаченный урок', **NULLABLE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='сумма оплаты')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name='способ оплаты')
+
+
+    def __str__(self):
+        return f'{self.user} - оплачено {self.course}, {self.lesson}.'
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
+
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
