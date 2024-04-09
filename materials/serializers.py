a16d78567e6c5088f703d2c457f02bd43bb867a6
SerjMonstrX <SerjMonstrX@gmail.com>
2024-04-09 21:45:43 +0700

    ДЗ 25_2 Доработка замечаний

--- materials/serializers.py
+++ materials/serializers.py
@@ -11,24 +11,6 @@ class LessonSerializer(serializers.ModelSerializer):
         validators = [VideoLinkValidator(field='video_link')]
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    lesson_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-    def get_lesson_count(self, instance):
-        return instance.lessons.count()  # lessons из модели Courses через related_name
-
-
-class SubscriptionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subscription
-        fields = ['user', 'course']
-
-
 class SubscriptionStatusSerializer(serializers.ModelSerializer):
     is_subscribed = serializers.SerializerMethodField()
 
@@ -39,3 +21,25 @@ class SubscriptionStatusSerializer(serializers.ModelSerializer):
     def get_is_subscribed(self, obj):
         user = self.context['request'].user
         return Subscription.objects.filter(user=user, course=obj.course).exists()
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+    subscription_status = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+    def get_lesson_count(self, instance):
+        return instance.lessons.count()  # lessons из модели Courses через related_name
+
+    def get_subscription_status(self, instance):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            user = request.user
+            subscription = Subscription.objects.filter(user=user, course=instance).first()
+            if subscription:
+                return SubscriptionStatusSerializer(subscription, context=self.context).data
+        return {'is_subscribed': False}  # Возвращаем по умолчанию False, если пользователь не авторизован
